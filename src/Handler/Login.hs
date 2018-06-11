f384d4d1685fcf5610e9236e8fde30332ef1439c
BlueReptile <callmekalango@gmail.com>
2018-06-10 22:40:22 -0300

    Implementação de AdminLoginR

--- src/Handler/Login.hs
+++ src/Handler/Login.hs
@@ -48,3 +48,47 @@ getLoginPageR = do
                       <i class="material-icons right">send</i>
         |]
         $(whamletFile "templates/footer.hamlet")
+
+
+
+
+
+postAdminLoginR :: Handler Html
+postAdminLoginR = do
+                 maybeId <- lookupSession "ID"
+                 idText <- case maybeId of
+                         (Just a) -> do
+                             return a
+                         _ -> do
+                             return ""
+                 login <- runInputPost $ ireq textField "f1"
+                 pass <- runInputPost $ ireq textField "f2"
+                 maybeAdmin <- runDB $ getBy $ UniqueLogin login pass
+                 case maybeAdmin of
+                             Just _ -> do
+                                        setSession "ID" $ login
+                                        redirect HomeR
+                             _ -> defaultLayout $ do
+                                 setTitle "ⓅⒶⒸ - Login"
+                                 addStylesheet $ (StaticR css_materialize_css)
+                                 addScript $ (StaticR js_jquery_js)
+                                 addScript $ (StaticR js_materialize_js)
+                                 toWidget $(juliusFile "templates/admin.julius")
+                                 toWidget $(luciusFile "templates/admin.lucius")
+                                 $(whamletFile "templates/header.hamlet")
+                                 toWidget $ [whamlet|
+                                   <main style="background-color: yellow; animation-name: piscadela; animation-duration: 1s; animation-iteration-count: infinite;">
+                                     <img src=@{StaticR sirene_gif} alt="ALERTA" class="center" style="width:200px;height:200px;">
+                                     <h1 class="center"> USUARIO INVALIDO
+
+                                 |]
+                                 toWidget $ [lucius|
+                                   @keyframes piscadela {
+                                       0%   {background-color:red;}
+                                       25%  {background-color:blue;}
+                                       50%  {background-color:red;}
+                                       75%  {background-color:blue;}
+                                       100% {background-color:red;}
+                                   }
+                                 |]
+                                 $(whamletFile "templates/footer.hamlet")
