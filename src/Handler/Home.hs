8a385046815db32a4d3b5a8ba1ddd255e5f0b80e
BlueReptile <callmekalango@gmail.com>
2018-06-10 21:22:34 -0300

    Implementação do HomeR

--- src/Handler/Home.hs
+++ src/Handler/Home.hs
@@ -11,6 +11,19 @@ import Text.Lucius
 
 getHomeR :: Handler Html
 getHomeR = do
+    maybeId <- lookupSession "ID"
+    idText <- case maybeId of
+            (Just id) -> do
+                return id
+            _ -> do
+                return ""
     defaultLayout $ do
+        setTitle "ⓅⒶⒸ - Home"
+        addStylesheet $ (StaticR css_materialize_css)
         addScript $ (StaticR js_jquery_js)
-        toWidget $[whamlet| Hello world |]
+        addScript $ (StaticR js_materialize_js)
+        toWidget $(juliusFile "templates/home.julius")
+        toWidget $(luciusFile "templates/home.lucius")
+        $(whamletFile "templates/header.hamlet")
+        $(whamletFile "templates/haskellchan.hamlet")
+        $(whamletFile "templates/footer.hamlet")
