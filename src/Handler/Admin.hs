6304cdc32b441e9b1ccfb915d7ceaa51bf29e989
BlueReptile <callmekalango@gmail.com>
2018-06-10 21:35:48 -0300

    Implementação do EditAdminR

--- src/Handler/Admin.hs
+++ src/Handler/Admin.hs
@@ -123,6 +123,52 @@ getADMPerfilR aid = do
                      <i class="material-icons right">send</i>
         |]
         $(whamletFile "templates/footer.hamlet")
+
+
+postEditAdminR :: Handler Html
+postEditAdminR = do
+     maybeId <- lookupSession "ID"
+     idText <- case maybeId of
+            (Just id) -> do
+                return id
+            _ -> do
+                redirect LoginPageR
+     aid <- runInputPost $ ireq hiddenField "aid"
+     admin <- runDB $ selectList [AdminId ==. aid] []
+     defaultLayout $ do
+        setTitle "ⓅⒶⒸ - Admin"
+        addStylesheet $ (StaticR css_materialize_css)
+        addScript $ (StaticR js_jquery_js)
+        addScript $ (StaticR js_materialize_js)
+        toWidget $(juliusFile "templates/admin.julius")
+        toWidget $(luciusFile "templates/admin.lucius")
+        $(whamletFile "templates/header.hamlet")
+        [whamlet|
+         <main>
+          <br>
+           <br>
+            <div class="row">
+              <div class="col s6 offset-s3 valign">
+               <form action=@{AltAdminR}  method=post>
+                <div class="card blue-grey darken-1">
+                 <div class="card-content white-text">
+                  <span class="card-title">ADMIN</span>
+                  <br>
+                  <p> Troca de Senha
+                  <br>
+                  <div class="input-field">
+                   <input type="hidden" id="aid" name="aid" value=#{fromSqlKey aid}>
+                   <input id="senha" name="senha" type="text" class="validate">
+                   <label class="white-text" for="senha">Senha</label>
+                 <br>
+                 <div class="card-action">
+                   <button class="btn waves-effect waves-light" type="submit" name="action">Editar
+                     <i class="material-icons right">send</i>
+        |]
+        $(whamletFile "templates/footer.hamlet")
+
+
+
 postAltAdminR :: Handler Html
 postAltAdminR = do
    maybeId <- lookupSession "ID"
